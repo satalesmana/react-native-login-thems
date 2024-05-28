4ae4d1e39c49526b3ccccaecce7ac3592a26aa51
muhammadshiddiq <mshiddiqs13@gmailcom>
2024-05-28 10:42:32 +0700

    tes keempat

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  WelcomeScreen,
+  RegisterScreen,
+  LoginScreen
+} from "./sata-lesmana"
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
