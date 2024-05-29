7207bdf38bd370d6f4f7dc9eb34498497d8adb81
SyawalAjg <mussyawal12@gmail.com>
2024-05-29 21:52:13 +0700

    update

--- app/screen/login/index.jsx
+++ app/screen/login/index.jsx
@@ -4,17 +4,21 @@ import {
   TextInput,
   StyleSheet,
   Button,
+  TouchableOpacity,
   ImageBackground,
   Dimensions,
   Image,
   ScrollView,
+  borderBottomColor,
 } from "react-native";
 import React from "react";
+
 const windowWidth = Dimensions.get("window").width;
+
 export default function LoginScreen() {
   const [email, onChangeEmail] = React.useState("");
   const [pasword, onChangePassword] = React.useState("");
-
+  const [number, onChangeNumber] = React.useState('');
   const onSubmitLogin = () => {
     alert(email);
   };
@@ -23,14 +27,14 @@ export default function LoginScreen() {
     <ScrollView>
       <View>
         <View style={{ width: windowWidth, height: 50 }}>
-          <View style={style.StatusBarStyle}>
-            <Image source={require("../../../assets/images/Status Bar.png")} />
-          </View>
+          
+          
 
           <View style={style.UserStyle1}>
             <Text style={[style.LoginStyle]}>Login</Text>
             <Image source={require("../../../assets/images/User.png")} />
           </View>
+
           <Text style={style.welcomebackStyle}>
             Welcome back , Rohit thakur
           </Text>
@@ -38,6 +42,7 @@ export default function LoginScreen() {
           <View style={style.gambarStyle}>
             <Image source={require("../../../assets/images/gambar.png")} />
           </View>
+
           <Text
             style={[
               style.textEnterStyle,
@@ -49,12 +54,45 @@ export default function LoginScreen() {
               },
             ]}
           >
-            ENTER YOUR MOBILE NUMBER
+            Enter Your Mobile Number
           </Text>
-        </View>
 
-        
+          <View>
+        <TextInput
+            style={style.textInputStyle}
+            onChangeText={onChangeNumber}
+            placeholder='Enter Number'
+            value={number}
+            keyboardType="numeric"/>
+          </View>
 
+          <Text
+            style={style.ChangeNumberStyle}>
+            Change Number?
+          </Text>
+          <TouchableOpacity
+          onPress={{}} style={style.buttonlogin}>
+            <Text style={style.title}>Login</Text>
+          </TouchableOpacity>
+            <Text style={style.OrLoginWith}>------------  Or Login With  ------------</Text>
+
+
+            <View style={{width:300, alignSelf:'center'}}>
+            <TouchableOpacity
+          onPress={{}} style={{height:25, flexDirection:'row',alignSelf:'center'}}>
+            <Image source={require('../../../assets/images/icons_google.png')} style={{width:20,height:20,marginVertical:'auto'}}>
+
+            </Image>
+            <Text style={{fontSize:20}}>Login</Text>
+          </TouchableOpacity>
+            </View>
+
+
+            <View>
+            <Text style={style.YouDonthaveanaccountSignup}>You Don't have an account ? <b>Sign up</b></Text>
+            </View>
+
+            </View>
       </View>
     </ScrollView>
   );
@@ -62,21 +100,30 @@ export default function LoginScreen() {
 
 const style = StyleSheet.create({
   gambarStyle: {
-    marginTop: 100,
+    marginTop: 50,
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 10,
   },
-  StatusBarStyle: {
-    marginTop: 0,
-    marginLeft: 20,
-    alignItems: "center",
-    justifyContent: "center",
+  buttonlogin:{
+    height: 40,
+    margin: 12,
+    padding: 10,
+    backgroundColor: "#F2796B",
+    borderRadius: 10,
+  },
+  // Button Login
+  title:{
+    textAlign:'center',
+    alignItems:'center',
+    justifyContent:'center',
+    color:'white',
+    fontSize: 15,
   },
   UserStyle1: {
     flex: 1,
     flexDirection: "row",
-    marginTop: 0,
+    marginTop: 20,
     marginLeft: 20,
   },
   LoginStyle: {
@@ -89,5 +136,40 @@ const style = StyleSheet.create({
     fontSize: 11,
     marginTop: 20,
     marginLeft: 20,
+  },
+  textInputStyle: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
+  },
+  ChangeNumberStyle: {
+    fontSize: 11,
+    textAlign: "right",
+    marginRight:30,
+    color:'#757171',
+  },
+  
+  ContainerLoginStyle: {
+    borderRadius: 10,
+    height: 40,
+    margin: 12,
+    padding: 10,
+    backgroundColor: "#F2796B",
+    borderRadius: 10,
+  },
+  OrLoginWith: {
+    textAlign:'center',
+    marginTop: 10 ,
+    color:'#757171',
+  },
+  YouDonthaveanaccountSignup:{
+    textAlign:'center',
+    marginTop: 70,
+    color:'#757171',
+  },
+  Signup:{
+    fontWeight:'bold',
   },
 });
