dfec76b8bd40e43a301de3ead81d89f355e76d60
yusufpras23 <prasetiayusuf700@gmail.com>
2024-06-06 20:07:27 +0700

    remote padding

--- app/screen/login/index.jsx
+++ app/screen/login/index.jsx
@@ -117,7 +117,7 @@ export default function LoginScreen({navigation}){
 
 const style = StyleSheet.create({
   container: { 
-    padding:10
+    padding:15
   },
   textInputStyle:{
     height: 50,
