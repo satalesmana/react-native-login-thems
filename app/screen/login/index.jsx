e3df87d2790f0cf63e93e3246ab3386993a9a1c7
RinandiArgoS <rinandiargo@gmail.com>
2024-06-07 17:33:51 +0700

    Update 3

--- app/screen/login/index.jsx
+++ app/screen/login/index.jsx
@@ -1,121 +1,148 @@
-import { View, Text, TextInput, StyleSheet, ScrollView, Alert, Dimensions, TouchableOpacity} from 'react-native';
-import {MyButton, MyButton2} from '../../components/my_button'
-import {ICGoogle} from '../../../assets'
-import React from 'react'
-
+import { View, Text, TextInput, StyleSheet, ScrollView, Alert, Dimensions, TouchableOpacity } from 'react-native';
+import { MyButton, MyButton2 } from '../../components/my_button'
+import { ICGoogle } from '../../../assets'
+import React, { useState } from 'react'
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const windowWidth = Dimensions.get('window').width;
 
-export default function LoginScreen({navigation}){
-const[email, onChangeEmail] = React.useState('')
-const[password, onChangePassword] = React.useState('')
+export default function LoginScreen({ navigation }) {
+    const [email, onChangeEmail] = React.useState('')
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const toogleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
 
+    const onSubmitLogin = () => {
+        try {
+            if (email.trim().length === 0) {
+                throw Error('Harap masukkan email anda')
+            }
+            if (password.trim().length === 0) {
+                throw Error('Harap masukkan password anda')
+            }
 
-const onSubmitLogin =()=>{
-    try{
-        if(email.trim().length === 0){
-            throw Error('Harap masukkan email anda')
+            navigation.navigate('home')
+        } catch (err) {
+            Alert.alert('Error', err.message, [
+                {
+                    text: 'OK', onPress: () => {
+                        console.log('ERR')
+                    }
+                },
+            ]);
         }
-        if(password.trim().length === 0){
-            throw Error('Harap masukkan password anda')
-        }
+    }
 
-    navigation.navigate('home')
-}catch (err){
-    Alert.alert('Error', err.message,[
-        {text: 'OK', onPress: () =>{
-            console.log('ERR')
-        }},
-    ]);
+    const onChangePass = () => {
+        navigation.navigate("#")
+    }
+
+    return (
+        <ScrollView>
+            <View>
+                {/* <View style={{ width: windowWidth, height: 400 }}> */}
+                <Text style={style.textLoginStyle}>Login</Text>
+                <View style={style.container}>
+                    <Text style={style.textLabel}>E-mail</Text>
+                    <TextInput
+                        style={style.textInputStyle}
+                        onChangeText={onChangeEmail}
+                        placeholder='Enter your email'
+                        placeholderTextColor='#b8b8ba'
+                        value={email} />
+
+                    <Text style={[style.textLabel, { marginTop: 20, marginBottom: 10 }]}>Password</Text>
+                    <View style={{ flex: 1, flexDirection: 'row', marginBottom: 10 }}>
+                        <TextInput
+                            style={[style.textInputStyle2]}
+                            onChangeText={setPassword}
+                            secureTextEntry={!showPassword}
+                            placeholder='Enter your password'
+                            placeholderTextColor='#b8b8ba'
+                            value={password} />
+                        <MaterialCommunityIcons
+                            name={showPassword ? "eye-off" : "eye"}
+                            size={25}
+                            color="#aaa"
+                            style={{ marginLeft: -40, marginTop: 15 }}
+                            onPress={toogleShowPassword} />
+                    </View>
+
+                    <TouchableOpacity onPress={onChangePass}>
+                        <Text style={[style.textLabel2]}>Forgot Password?</Text>
+                    </TouchableOpacity>
+
+                </View>
+
+                <View style={{width:'90%', alignSelf:'center'}}>
+                <MyButton2 onPress={onSubmitLogin} text="Login"></MyButton2>
+                </View>
+
+            </View>
+            <View style={style.container2}>
+                <Text style={style.textContinueStyle}>or login with</Text>
+                <View>
+                    <MyButton text="Login with Google" imgUrl={ICGoogle} />
+                </View>
+            </View>
+            {/* </View> */}
+        </ScrollView>
+    );
 }
-}
 
-const onChangePass=()=>{
-    navigation.navigate("#")
-}
-
-return(
-<ScrollView>
-<View>
-    <View style={{width: windowWidth, height:400}}>
-    <Text style={style.textLoginStyle}>Login</Text>
-        <View style={style.container}>
-            <Text style={style.textLabel}>E-mail</Text>
-            <TextInput 
-            style={style.textInputStyle}
-            onChangeText={onChangeEmail}
-            placeholder='Enter your email'
-            placeholderTextColor='#b8b8ba'
-            value={email}/>
-
-            <Text style={[style.textLabel, {marginTop:20}]}>Password</Text>
-            <TextInput 
-            style={[style.textInputStyle, {marginBottom:12}]}
-               onChangeText={onChangePassword} 
-               placeholder='Enter your password'
-               placeholderTextColor ='#b8b8ba'
-               value ={password}/>
-            <TouchableOpacity  onPress={onChangePass}>
-            <Text style={[style.textLabel2]}>Forgot Password?</Text>
-            </TouchableOpacity>
-
-            <MyButton2 onPress={onSubmitLogin} text="Login"></MyButton2>
-        </View>
-    </View>
-<View style={style.container2}>
-    <Text style={style.textContinueStyle}>or login with</Text>
-        <View>
-    <MyButton text="Login with Google" imgUrl={ICGoogle}/>
-        </View>
-    </View>
-</View>
-    </ScrollView>
-);
-}
-
-const style= StyleSheet.create({
+const style = StyleSheet.create({
     container: {
-        padding:20,
-        marginTop:20
+        padding: 20,
+        marginTop: 20
     },
     container2: {
-        padding:20,
-        marginTop:380
+        padding: 20,
+        marginTop: 230
     },
-    textInputStyle:{
-        height:50,
-        marginTop:12,
-        borderWidth:1.5,
-        borderRadius:10,
-        borderColor:'#b8b8ba',
-        padding:10,
+    textInputStyle: {
+        height: 50,
+        marginTop: 12,
+        borderWidth: 1.5,
+        borderRadius: 10,
+        borderColor: '#b8b8ba',
+        padding: 10,
     },
-    textLoginStyle:{
-        fontSize:32,
-        marginTop:100,
-        fontWeight:'bold',
-        textAlign:'center'
+    textInputStyle2: {
+        height: 50,
+        width: '100%',
+        borderWidth: 1.5,
+        borderRadius: 10,
+        borderColor: '#b8b8ba',
+        padding: 10,
     },
-    brandStyle:{
-        marginTop:100,
-        alignItems:'center',
-        justifyContent:'center'
+    textLoginStyle: {
+        fontSize: 32,
+        marginTop: 100,
+        fontWeight: 'bold',
+        textAlign: 'center'
     },
-    textLabel:{
-        fontSize:12,
-        fontWeight:'bold'
+    brandStyle: {
+        marginTop: 100,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    textLabel2:{
-        fontSize:12,
-        fontWeight:'bold',
-        textAlign:"right",
-        color:"#4397f7",
-        textDecorationLine:"underline",
-        marginBottom:60
+    textLabel: {
+        fontSize: 12,
+        fontWeight: 'bold'
     },
-    textContinueStyle:{
+    textLabel2: {
+        fontSize: 12,
+        fontWeight: 'bold',
+        textAlign: "right",
+        color: "#4397f7",
+        textDecorationLine: "underline",
+        marginBottom: 70
+    },
+    textContinueStyle: {
         textAlign: 'center',
-        color:"#8f8e8d",
-        marginBottom:20
+        color: "#8f8e8d",
+        marginBottom: 20
     }
 })
