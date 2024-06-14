252278eaec48bfa2ebb9171052261232cbcaf1b9
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-06-14 14:47:49 +0700

    update App

--- app/screen/index.jsx
+++ app/screen/index.jsx
@@ -2,10 +2,12 @@ import WelcomeScreen from "./welcome";
 import CreateScreen from "./register";
 import LoginScreen from "./login";
 import HomeScreen from "./home";
+import ProfileScreen from "./profile";
 
 export {
     WelcomeScreen,
     CreateScreen,
     LoginScreen,
-    HomeScreen
+    HomeScreen,
+    ProfileScreen
 }
