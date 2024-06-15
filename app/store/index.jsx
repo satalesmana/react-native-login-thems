bec138002abd1d1d2cecee5a84a251f916ba083a
Oioioifatih <nzwaft@gmail.com>
2024-06-15 14:55:41 +0700

    update setting

--- app/store/index.jsx
+++ app/store/index.jsx
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import registerReducer from './reducer/registerReducer'
 import usersReducer from "./reducer/usersReducer"
 import authReducer from './reducer/authReducer'
+import settingReducer from './reducer/settingReducer'
 export default configureStore({
   reducer: {
     register: registerReducer,
     users: usersReducer,
-    auth:authReducer
+    auth:authReducer,
+    settings:settingReducer
   },
 })
