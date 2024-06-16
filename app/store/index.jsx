8507e8da2ee8a9cb6d4c8d2dd0146b7df812749e
hidayatnurwahid <nurwahidh55@gmail.com>
2024-06-16 08:56:10 +0700

    create form list in page homescreen

--- app/store/index.jsx
+++ app/store/index.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import registerReducer from './reducer/registerReducer'
-import authReducer from "./reducer/authReducer"
+import authReducer from './reducer/authReducer'
+import usersReducer from './reducer/usersReducer'
 
 export default configureStore({
   reducer: {
     register: registerReducer,
-    auth: authReducer
+    auth: authReducer,
+    users: usersReducer
   },
 })
